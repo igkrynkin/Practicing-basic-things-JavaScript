000b8b5fc578cafd9e777d0f8dbb5d7046cc98f3
Igor Krynkin <igor.krynkin@gmail.com>
2023-09-20 16:35:32 +0300

    Study JS-3

--- Study/WWW-JS/js/main.js
+++ Study/WWW-JS/js/main.js
@@ -1,5 +1,111 @@
-/* document.write('Hello world!');
-         write - выводит на прямую в документ. */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                // Урок 2
+
+
+
+// /* document.write('Hello world!');
+//          write - выводит на прямую в документ. */
          
-         console.log('Hello world!');
-         // console - встроенный объект к которому мы обращаемся, а внутри этого объекта существуют некоторые функции, как например внутри объекта document существовала функция write, так и внутри объекта console существует функция log к которой мы можем обратиться 
+//          console.log('Hello world!');
+//          // console - встроенный объект к которому мы обращаемся, а внутри этого объекта существуют некоторые функции, как например внутри объекта document существовала функция write, так и внутри объекта console существует функция log к которой мы можем обратиться 
+
+
+
+
+                                                // Урок 3
+
+// var num;
+// num = 5;
+
+// var num = 5;
+// num = 7;
+// console.log("Переменная " + num + ".");
+
+
+
+
+                                                // Урок 4
+
+// var num_1 = 5;
+// var num_2 = 15;
+
+// var res = num_1 - num_2;
+
+// console.log('Вычитание: ' + res);
+
+
+
+                                                // Урок 5
+
+var num_1 = 5;
+var num_2 = 15;
+
+var res = num_1 - num_2;
+
+console.log('Результат: ' + (num_1 - num_2));
+
+
+var num_3 = 5;
+num_3 = num_3 + 7;
+console.log('Результат: ' + num_3);
+
+
+// var num_3 = 5;
+// num_3 += 1;    добавляет еденицу, также можно записать num_3++
+// console.log('Результат: ' + num_3);
+
+
+var str_1 = '12';
+var str_2 = '2';
+console.log('Результат: ' + (str_1 + str_2));
+
+
+var str_1 = Number('12');
+var str_2 = Number('2');
+console.log('Результат: ' + (str_1 + str_2));
+
+
+console.log('Math: ' + Math.PI);
+console.log('Math: ' + Math.E);
+console.log('Math: ' + Math.max(4, 67, -8, 2, 0, 5, 8));
