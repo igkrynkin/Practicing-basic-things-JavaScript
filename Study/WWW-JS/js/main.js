2f574c9d0d923b01009a41fcf2726fd6255be36b
Igor Krynkin <igor.krynkin@gmail.com>
2023-11-07 14:20:56 +0300

    Study JS-12-Form-1

--- Study/WWW-JS/js/main.js
+++ Study/WWW-JS/js/main.js
@@ -1,111 +1,27 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                                // Урок 2
-
-
-
-// /* document.write('Hello world!');
-//          write - выводит на прямую в документ. */
-         
-//          console.log('Hello world!');
-//          // console - встроенный объект к которому мы обращаемся, а внутри этого объекта существуют некоторые функции, как например внутри объекта document существовала функция write, так и внутри объекта console существует функция log к которой мы можем обратиться 
-
-
-
-
-                                                // Урок 3
-
-// var num;
-// num = 5;
-
-// var num = 5;
-// num = 7;
-// console.log("Переменная " + num + ".");
-
-
-
-
-                                                // Урок 4
-
-// var num_1 = 5;
-// var num_2 = 15;
-
-// var res = num_1 - num_2;
-
-// console.log('Вычитание: ' + res);
-
-
-
-                                                // Урок 5
-
-var num_1 = 5;
-var num_2 = 15;
-
-var res = num_1 - num_2;
-
-console.log('Результат: ' + (num_1 - num_2));
-
-
-var num_3 = 5;
-num_3 = num_3 + 7;
-console.log('Результат: ' + num_3);
-
-
-// var num_3 = 5;
-// num_3 += 1;    добавляет еденицу, также можно записать num_3++
-// console.log('Результат: ' + num_3);
-
-
-var str_1 = '12';
-var str_2 = '2';
-console.log('Результат: ' + (str_1 + str_2));
-
-
-var str_1 = Number('12');
-var str_2 = Number('2');
-console.log('Результат: ' + (str_1 + str_2));
-
-
-console.log('Math: ' + Math.PI);
-console.log('Math: ' + Math.E);
-console.log('Math: ' + Math.max(4, 67, -8, 2, 0, 5, 8));
+function checkForm(el) {
+
+    var name = el.name.value;
+    var pass = el.pass.value;
+    var repass = el.repass.value;
+    var state = el.state.value;
+
+
+    var fail = "";
+
+    if (name == "" || pass == "" || state == "")
+        fail = "Заполните все поля";
+    else if(name.length <= 1 || name.length > 50)
+        fail = "Введите корректное имя";
+    else if(pass != repass)
+        fail = "Пароли должны совпадать";
+    else if(pass.split("&").length > 1)
+        fail = "Некорректный пароль";
+
+    if(fail != "") {
+        document.getElementById('error').innerHTML = fail;
+    } else {
+        alert("Все данные корректно заполнены");
+        window.location = "https://ya.ru"; 
+    }
+    return false;
+}
