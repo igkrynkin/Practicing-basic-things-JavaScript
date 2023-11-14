c61d2934ec15d7646823c6530523ebf48f3873b7
Igor Krynkin <igor.krynkin@gmail.com>
2023-11-14 16:12:06 +0300

    Study JavaScript forEach-3

--- Study/WWW-JS/js/main.js
+++ Study/WWW-JS/js/main.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('.btn').forEach(function(btn) {
     btn.addEventListener('click', function() {
-        alert(btn.dataset.numberString)
+        alert(`Кнопка ${btn.dataset.number}`)
     })
 });
