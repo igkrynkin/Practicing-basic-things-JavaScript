06a56c1d11cf1e1bc6c244cb138af1d8cd697758
Igor Krynkin <igor.krynkin@gmail.com>
2023-11-15 15:56:54 +0300

    Study JS-12-Form-4

--- Study/WWW-JS/js/main.js
+++ Study/WWW-JS/js/main.js
@@ -1,5 +1,14 @@
 document.querySelectorAll('.btn').forEach(function(btn) {
     btn.addEventListener('click', function() {
+        let obj = {
+            name: "Илья",
+            age: 17
+        }
+
+        btn.dataset.user = JSON.stringify(obj)
+
         alert(`Кнопка ${btn.dataset.number}, кликов: ${ ++btn.dataset.clicks}`)
+
+        console.log(JSON.parse(btn.dataset.user));
     })
 });
