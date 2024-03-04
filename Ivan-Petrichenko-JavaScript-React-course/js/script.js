003f67d42da5eef2c002d692655f5184dfb754ee
Igor Krynkin <igor.krynkin@gmail.com>
2024-03-04 14:33:50 +0300

    Ivan-Petrichenko

--- Ivan-Petrichenko-JavaScript-React-course/js/script.js
+++ Ivan-Petrichenko-JavaScript-React-course/js/script.js
@@ -1,4 +1,6 @@
-/* Задание на урок:
+// ####################################################### Задача №1 ##################################
+
+/* Задание на урок №1:
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
 'Сколько фильмов вы уже посмотрели?'
@@ -22,7 +24,7 @@
 Проверить, чтобы все работало без ошибок в консоли */
 
 
-// ####################################################### Мой ответ ##################################
+// ####################################################### Мой ответ №1 ##################################
 
 // 'use strict';
 
@@ -62,26 +64,118 @@
 
 
 
-// ####################################################### Ответ урока ##################################
+// ####################################################### Ответ урока №1 ##################################
+
+// 'use strict';
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+    
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+    
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+    
+// console.log(personalMovieDB);
+
+
+// ####################################################### Задача/ОТВЕТ №2 ##################################
+
+/* Задание Логические операторы:
+
+https://docs.google.com/document/d/1RDxwMg7pSI9QzhYU0sY59kyspI71r-3_hzJoSbHRk0I/edit?pli=1
+
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const hamburger = 2;
+const fries = 1;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-    
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-    
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-    
-console.log(personalMovieDB);
+if (hamburger === 3 && fries) {
+    console.log('Все сыты!');
+} else {
+    console.log('Мы уходим');
+}
+
+############## 1 #################
+
+Что выведет в консоль код?
+
+
+console.log( NaN || 2 || undefined );  выведет 2 (первое ложное значение)
+
+console.log( NaN && 2 && undefined );  выведет NaN (первое ложное значение)
+
+console.log( 1 && 2 && 3 );  выведет 3, так как все истинные, то выводит крайний
+
+console.log( !1 && 2 || !3 );    выведет 2, так как || возвращает первое истинное значение
+
+console.log( 25 || null && !3 );  выведет 25
+
+console.log( NaN || null || !3 || undefined || 5);   выведет 5
+
+console.log( NaN || null && !3 && undefined || 5);   выведет 5
+
+console.log( 5 === 5 && 3 > 1 || 5);    выведет true
+
+
+############## 2 #################
+
+Выполняется ли условие?
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
+Условия выполнятся, так-как false || true = true.
+
+
+
+############## 3 #################
+
+Выполняется ли условие?
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!')
+}
+
+Условия выполнятся, nuggets = true.
+
+
+Выполняется ли условие?
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
+Условия не выполнятся.
+
+*/
