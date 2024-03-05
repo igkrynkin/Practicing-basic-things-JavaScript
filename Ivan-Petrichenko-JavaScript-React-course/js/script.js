9e0d518b7242fd70731f4f7e96ad1a2f233e5ed0
Igor Krynkin <igor.krynkin@gmail.com>
2024-03-05 15:52:35 +0300

    Ivan-Petrichenko-3

--- Ivan-Petrichenko-JavaScript-React-course/js/script.js
+++ Ivan-Petrichenko-JavaScript-React-course/js/script.js
@@ -183,18 +183,33 @@ if (hamburger && cola || fries === 3 && nuggets) {
 
 // ####################################################### Вложенный цикл ##################################
 
+// 'use strict';
+
+// let result = '';
+// const length = 7;
+
+// for (let i = 1; i < length; i++) {
+
+//     for (let j = 0; j < i; j++) {
+//         result += "*";
+//     }
+
+//     result += '\n';
+// }
+
+// console.log(result);
+
+// ####################################################### Цикл label ##################################
+
 'use strict';
 
-let result = '';
-const length = 7;
-
-for (let i = 1; i < length; i++) {
-
-    for (let j = 0; j < i; j++) {
-        result += "*";
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) continue first;
+            console.log(`Third level: ${k}`);
+        }
     }
-
-    result += '\n';
 }
-
-console.log(result);
