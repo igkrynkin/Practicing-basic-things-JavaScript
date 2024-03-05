c5d74788ddb599e370fae3796dff624b147eb439
Igor Krynkin <igor.krynkin@gmail.com>
2024-03-05 14:36:25 +0300

    Ivan-Petrichenko-2

--- Ivan-Petrichenko-JavaScript-React-course/js/script.js
+++ Ivan-Petrichenko-JavaScript-React-course/js/script.js
@@ -179,3 +179,22 @@ if (hamburger && cola || fries === 3 && nuggets) {
 Условия не выполнятся.
 
 */
+
+
+// ####################################################### Вложенный цикл ##################################
+
+'use strict';
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+
+    result += '\n';
+}
+
+console.log(result);
